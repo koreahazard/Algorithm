41613431880b1996019796cbd46c5ef3c6b76a03
이형준 <118117354+koreahazard@users.noreply.github.com>
2025-05-13 13:06:58 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.02 ms, Memory: 73.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String solution(String s) {
+        int len = s.length();
+        if (len % 2 == 0) {
+        
+            return s.substring(len/2 - 1, len/2 + 1);
+        } else {
+            
+            return String.valueOf(s.charAt(len / 2));
+        }
+    }
+}
