fb368a1fe3c5b56d89b0c6153daa46e5efa97490
이형준 <118117354+koreahazard@users.noreply.github.com>
2025-06-08 14:39:57 +0900

    [Bronze V] Title: A+B, Time: 172 ms, Memory: 17720 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); // Scanner를 이용해 입력 받기
+        int A = sc.nextInt(); // 첫 번째 정수 입력
+        int B = sc.nextInt(); // 두 번째 정수 입력
+        System.out.println(A + B); // A + B 결과 출력
+    }
+}
